c93eafe4ff48a337094fc4a429113e797e558f18
shailendra <shailendracheruku1611@gmail.com>
2024-09-17 12:30:39 +0530

    react basic and first count project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+
+   const username='shailu'
+
+
+ return (
+   <>
+   
+   <h1> React with cac in vite | shailu</h1>
+   <p>test</p>
+
+   <h1>chai aur react {username}</h1>
+   
+   </>
+ 
+ )
+}
+
+export default App
