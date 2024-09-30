616b4256aa194d25a1d6ae3601bdd01e5a6c22f4
shailendra <shailendracheruku1611@gmail.com>
2024-09-30 22:46:09 +0530

    react router crash course

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+  const {userid}=useParams()
+  return (
+    
+    <div className='bg-orange-700'>
+      User: {userid}
+    </div>
+  )
+}
+
+export default User
